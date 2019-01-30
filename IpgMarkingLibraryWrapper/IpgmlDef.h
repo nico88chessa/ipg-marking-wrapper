621b4b115e9b9311d50828ad020bcec1800bea12
Nicola Chessa <n.chessa@dvoptic.com>
2019-01-30 10:10:31 +0100

    clean repository;

--- IpgMarkingLibraryWrapper/IpgmlDef.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef IPGMARKINGLIBRARYWRAPPER_EXPORTS
-#define IPGMARKINGLIBRARYWRAPPER_API __declspec(dllexport)
-#else
-#define IPGMARKINGLIBRARYWRAPPER_API __declspec(dllimport)
-#endif
